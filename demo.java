8af6347944b467adb69b37bd65fe19b6e802694c
Shriya <sanand20@stuy.edu>
2020-10-20 21:32:23 -0400

    I tested this program and it works now

---
+++ demo.java
@@ -0,0 +1,18 @@
+public class demo {
+  public static void main(String[] args){
+  int n=0;
+  if (args.length<1){
+    n=5;
+  }
+  else {
+    n = Integer.parseInt(args[0]);
+  }
+  for(int i=1; i<=n; i++){
+    for(int j=0; j<=n-i; j++){
+       System.out.print(i);
+    }
+    System.out.println();
+  }
+}
+
+}
